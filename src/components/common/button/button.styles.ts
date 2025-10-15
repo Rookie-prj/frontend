8b8031f43000344159c77ce7a8bc1e6ead2e4efc
heeyoung123 <heeyoung321@naver.com>
2025-10-15 14:57:50 +0900

    design: 버튼 폰트 색상 수정

--- src/components/common/button/button.styles.ts
+++ src/components/common/button/button.styles.ts
@@ -3,10 +3,17 @@ import { colors } from '../../../style/colors';
 
 export const ButtonContainer = styled.button`
   background-color: ${colors.green[200]};
-  color: ${colors.white};
+  color: ${colors.gray[800]};
   border-radius: 0.75rem;
   display: flex;
   width: 100%;
+
+  text-align: center;
+
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.25rem;
   height: 3.125rem;
   padding: 1rem 9.875rem;
   justify-content: center;
