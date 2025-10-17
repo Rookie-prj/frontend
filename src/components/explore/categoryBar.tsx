f6ec6214f3f298c98a4a77fa2ca4b8d812eb66ad
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-17 17:48:46 +0900

    refactor: 불필요한 코드 삭제

--- src/components/explore/categoryBar.tsx
+++ src/components/explore/categoryBar.tsx
@@ -1,5 +1,5 @@
 import QueryLink from '../../components/common/queryLink';
-import { EXPLORE_CATEGORIES, ExploreCategoryValue } from '../../constants/category';
+import CATEGORY, { ExploreCategoryValue } from '../../constants/category';
 import {
   CategoryBarContainer,
   CategoryBarItem,
@@ -12,25 +12,25 @@ interface CategoryBarProps {
 }
 
 function CategoryBar({ sortType }: CategoryBarProps) {
-  const currentSortType = sortType || 'project';
+  const currentSortType = sortType || CATEGORY.PROJECT.value;
 
   return (
     <CategoryBarContainer>
       <ItemWrapper>
-        <QueryLink extraQuery={{ sortType: 'project' }} preserveQuery={false}>
-          <CategoryBarItem $isActive={currentSortType === 'project'}>
-            <CategoryBarText $isActive={currentSortType === 'project'}>
-              {EXPLORE_CATEGORIES.PROJECT.label}
+        <QueryLink extraQuery={{ sortType: CATEGORY.PROJECT.value }} preserveQuery={false}>
+          <CategoryBarItem $isActive={currentSortType === CATEGORY.PROJECT.value}>
+            <CategoryBarText $isActive={currentSortType === CATEGORY.PROJECT.value}>
+              {CATEGORY.PROJECT.label}
             </CategoryBarText>
           </CategoryBarItem>
         </QueryLink>
       </ItemWrapper>
 
       <ItemWrapper>
-        <QueryLink extraQuery={{ sortType: 'rookie' }} preserveQuery={false}>
-          <CategoryBarItem $isActive={currentSortType === 'rookie'}>
-            <CategoryBarText $isActive={currentSortType === 'rookie'}>
-              {EXPLORE_CATEGORIES.ROOKIE.label}
+        <QueryLink extraQuery={{ sortType: CATEGORY.ROOKIE.value }} preserveQuery={false}>
+          <CategoryBarItem $isActive={currentSortType === CATEGORY.ROOKIE.value}>
+            <CategoryBarText $isActive={currentSortType === CATEGORY.ROOKIE.value}>
+              {CATEGORY.ROOKIE.label}
             </CategoryBarText>
           </CategoryBarItem>
         </QueryLink>
--- src/constants/category.ts
+++ src/constants/category.ts
@@ -12,13 +12,9 @@ export const CATEGORY_GROUPS = {
   SEARCH: [CATEGORY.PROJECT, CATEGORY.ROOKIE],
   PROFILE: [CATEGORY.SAVED, CATEGORY.MY_PROJECT],
 } as const;
-export const EXPLORE_CATEGORIES = {
-  PROJECT: { value: 'project', label: '프로젝트' },
-  ROOKIE: { value: 'rookie', label: '루키' },
-} as const;
 
+export type ExploreCategoryValue = (typeof CATEGORY_GROUPS.SEARCH)[number]['value'];
 export type CategoryValue = (typeof CATEGORY)[keyof typeof CATEGORY]['value'];
 export type CategoryGroup = keyof typeof CATEGORY_GROUPS;
-export type ExploreCategoryValue =
-  (typeof EXPLORE_CATEGORIES)[keyof typeof EXPLORE_CATEGORIES]['value'];
+
 export default CATEGORY;
