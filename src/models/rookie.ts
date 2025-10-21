389e888c4e412e0c95200c75ca94d838d6fe82bb
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-22 00:51:15 +0900

    feat: 탐색페이지 무한스크롤 구현

---
+++ src/models/rookie.ts
@@ -0,0 +1,21 @@
+export interface Rookie {
+  userId: number;
+  currentStudy: string;
+  emailId: string;
+  passwordHash: string;
+  name: string;
+  universityName: string;
+  schoolPublicFlag: number;
+  major: string;
+  grade: string;
+  currentStudyDetail: string;
+  toolset: string;
+  favoriteSubject: string;
+  publicPortfolioCount: string;
+  recruitPeople: string;
+  responseRate: string;
+  passionMeter: string;
+  profileImageUrl: string;
+}
+
+export type RookieList = Rookie[];
