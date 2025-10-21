4525484f2482949b3e1c96790df7ea4d04bd5b82
heeyoung123 <heeyoung321@naver.com>
2025-10-21 15:02:36 +0900

    feat :postDetail의  authorSection 추가

--- src/pages/postDetail/postDetail.tsx
+++ src/pages/postDetail/postDetail.tsx
@@ -4,21 +4,6 @@ import {
   PostDetailContainer,
   HeaderImage,
   ContentContainer,
-  AuthorSection,
-  AuthorTitle,
-  AuthorCard,
-  AuthorProfile,
-  AuthorInfo,
-  AuthorName,
-  AuthorVerification,
-  AuthorLocation,
-  AuthorStats,
-  AuthorStatItem,
-  AuthorStatValue,
-  AuthorStatLabel,
-  AuthorLevel,
-  LevelIcon,
-  LevelText,
   ProjectDescription,
   DescriptionTitle,
   DescriptionContent,
@@ -45,6 +30,7 @@ import StatusChips from '../../components/post/statusChips/statusChips';
 import TitleSection from '../../components/post/postDetail/titleSection';
 import InfoSection from '../../components/post/postDetail/infoSection';
 import PositionSection from '../../components/post/postDetail/positionSection';
+import AuthorSection from '../../components/post/postDetail/authorSection';
 const PostDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const postData = mockPostData.find((post) => post.id === Number(id)) || mockPostData[0];
@@ -86,83 +72,13 @@ const PostDetail: React.FC = () => {
         <PositionSection total={postData.recruitment.total} positions={postData.positions} />
 
         {/* 작성자 정보 */}
-        <AuthorSection>
-          <AuthorTitle>작성자 루키 정보</AuthorTitle>
-          <AuthorCard>
-            <AuthorProfile>
-              <div className="profile-image">
-                <svg width="66" height="66" viewBox="0 0 66 66" fill="none">
-                  <circle cx="33" cy="33" r="33" fill={colors.gray[200]} />
-                  <circle cx="33" cy="26" r="8" fill={colors.gray[400]} />
-                  <path
-                    d="M20 50C20 42.268 25.268 36 33 36C40.732 36 46 42.268 46 50"
-                    fill={colors.gray[400]}
-                  />
-                </svg>
-              </div>
-              <AuthorInfo>
-                <div className="name-section">
-                  <AuthorName>{postData.authorInfo.name}</AuthorName>
-                  {postData.authorInfo.verified && (
-                    <AuthorVerification>
-                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-                        <path
-                          d="M8 0L10.5 5.5L16 6L12 10L13 16L8 13L3 16L4 10L0 6L5.5 5.5L8 0Z"
-                          fill={colors.gray[400]}
-                        />
-                      </svg>
-                      <span>학교 인증완료</span>
-                    </AuthorVerification>
-                  )}
-                </div>
-                <AuthorLocation>
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path
-                      d="M8 0C5.2 0 3 2.2 3 5C3 9 8 16 8 16S13 9 13 5C13 2.2 10.8 0 8 0ZM8 6.5C6.6 6.5 5.5 5.4 5.5 4C5.5 2.6 6.6 1.5 8 1.5C9.4 1.5 10.5 2.6 10.5 4C10.5 5.4 9.4 6.5 8 6.5Z"
-                      fill={colors.gray[400]}
-                    />
-                  </svg>
-                  <span>{postData.authorInfo.location}</span>
-                </AuthorLocation>
-              </AuthorInfo>
-            </AuthorProfile>
-            <AuthorStats>
-              <AuthorStatItem>
-                <AuthorStatValue>{postData.authorInfo.projects}개</AuthorStatValue>
-                <AuthorStatLabel>공개 가능 프로젝트</AuthorStatLabel>
-              </AuthorStatItem>
-              <AuthorStatItem>
-                <AuthorStatValue>{postData.authorInfo.responseRate}%</AuthorStatValue>
-                <AuthorStatLabel>응답률</AuthorStatLabel>
-              </AuthorStatItem>
-              <AuthorLevel>
-                <LevelIcon>
-                  <svg width="51" height="27" viewBox="0 0 51 27" fill="none">
-                    <rect width="51" height="27" rx="13.5" fill={colors.green[200]} />
-                    <rect x="0" y="8" width="33" height="19" rx="9.5" fill={colors.green[300]} />
-                    <path
-                      d="M38 18L42 14L46 18"
-                      stroke={colors.gray[800]}
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </LevelIcon>
-                <LevelText>
-                  <span>{postData.authorInfo.level}</span>
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none">
-                    <path
-                      d="M6 0L7.5 3L10.5 3L8.25 5.25L9 8.25L6 6.75L3 8.25L3.75 5.25L1.5 3L4.5 3L6 0Z"
-                      fill={colors.gray[800]}
-                    />
-                  </svg>
-                </LevelText>
-              </AuthorLevel>
-            </AuthorStats>
-          </AuthorCard>
-        </AuthorSection>
-
+        <AuthorSection
+          name={postData.authorInfo.name}
+          location={postData.authorInfo.location}
+          projects={postData.authorInfo.projects}
+          responseRate={postData.authorInfo.responseRate}
+          level={postData.authorInfo.level}
+        />
         {/* 프로젝트 설명 */}
         <ProjectDescription>
           <DescriptionTitle>프로젝트 소개</DescriptionTitle>
