fc59af9afa7b8f92dc31d7c50ab039190e0df015
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-23 17:05:02 +0900

    refactor: 네이밍 변경 search -> explore

--- src/components/explore/projectCard.tsx
+++ src/components/explore/projectCard.tsx
@@ -10,10 +10,22 @@ interface ProjectCardProps {
 }
 
 function ProjectCard({ project, rightIcon }: ProjectCardProps) {
+  // D-day 계산
+  const calculateDday = (endDate: string) => {
+    const today = new Date();
+    const end = new Date(endDate);
+    const diffTime = end.getTime() - today.getTime();
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays > 0 ? `D-${diffDays}` : '마감';
+  };
+
+  // 첫 번째 이미지 또는 기본 이미지
+  const projectImage = project.imageUrl1 || backgroundImg;
+
   return (
     <S.ProjectCardContainer>
       <S.ProjectImageWrapper>
-        <S.ProjectImage src={backgroundImg} alt="프로젝트 이미지" />
+        <S.ProjectImage src={projectImage} alt="프로젝트 이미지" />
         <S.ImageDimOverlay />
 
         <S.IconButtonWrapper iconSrc={rightIcon}></S.IconButtonWrapper>
@@ -25,20 +37,20 @@ function ProjectCard({ project, rightIcon }: ProjectCardProps) {
         <S.ProfileImage src={profile} alt="작성자" />
 
         <S.TagsWrapper>
-          <S.Tag textColor="#1E2939">D-7</S.Tag>
-          <S.Tag textColor="#364153">처음부터 시작</S.Tag>
+          <S.Tag textColor="#1E2939">{calculateDday(project.endDate)}</S.Tag>
+          <S.Tag textColor="#364153">{project.distance}</S.Tag>
         </S.TagsWrapper>
       </S.ProjectImageWrapper>
 
       <S.ContentWrapper>
         <S.InfoRow>
           <S.InfoItem>
-            <S.InfoIcon src={member} alt="멤버수" />
-            <S.InfoText>{project.viewCount}</S.InfoText>
+            <S.InfoIcon src={member} alt="지원자" />
+            <S.InfoText>{project.requredPpl}명</S.InfoText>
           </S.InfoItem>
           <S.InfoItem>
-            <S.InfoIcon src={position} alt="포지션" />
-            <S.InfoText>{project.boardType}</S.InfoText>
+            <S.InfoIcon src={position} alt="조회수" />
+            <S.InfoText>조회 {project.viewCount}</S.InfoText>
           </S.InfoItem>
         </S.InfoRow>
 
@@ -49,9 +61,9 @@ function ProjectCard({ project, rightIcon }: ProjectCardProps) {
             필수 툴
           </S.DetailLabel>
           <S.DetailContent>
-            {project.cowrkrPosition}
+            {project.cowrkrPosition.join(', ')}
             <br />
-            {project.cowrkrSpeciality}
+            {project.workTools?.join(', ') || project.techTools}
           </S.DetailContent>
         </S.DetailSection>
       </S.ContentWrapper>
