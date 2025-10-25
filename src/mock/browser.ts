be3caa08887fd4bb1f5af6de627a3b7d0958d719
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-26 01:29:59 +0900

    feat: 라이브러리 모킹 함수 구현

--- src/mock/browser.ts
+++ src/mock/browser.ts
@@ -3,7 +3,9 @@ import { ex } from './ex';
 import { rookie } from './rookie';
 import { project } from './project';
 import { postUserCheerup } from './cheerup';
+import { savedBoards } from './savedBoards';
+import { myProjectBoards } from './myProject';
 
-const handlers = [ex, rookie, project, postUserCheerup];
+const handlers = [ex, rookie, project, postUserCheerup, savedBoards, myProjectBoards];
 
 export const worker = setupWorker(...handlers);
