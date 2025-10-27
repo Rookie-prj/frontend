3786a4e509d239a084d46be9fab709a5a24dae0d
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-28 01:28:02 +0900

    feat: 프로필 모델 추가

--- src/api/myProfile.ts
+++ src/api/myProfile.ts
@@ -1,10 +1,6 @@
 import { apiClient } from './index';
 import { API_ENDPOINT } from '../constants/apiEndpoint';
-import { MyProfileBoard, MyProfileDetail } from '../models/myProfile';
-
-export const getMyProfileBoards = (writer: string) => {
-  return apiClient.get<MyProfileBoard[]>(`${API_ENDPOINT.LIBRARY_MY_PROJECT}?writer=${writer}`);
-};
+import { MyProfileDetail } from '../models/myProfile';
 
 export const getMyProfileDetail = () => {
   return apiClient.get<MyProfileDetail>(API_ENDPOINT.LIBRARY_MY_DETAIL);
