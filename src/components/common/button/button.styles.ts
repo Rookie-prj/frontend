92336fe9ff0b94c2e3ffe803cdc314c5e6e33036
heeyoung123 <heeyoung321@naver.com>
2025-10-28 14:48:25 +0900

    design : 히어로 배너 3번쨰 슬라이드 추가, 2번째, 3번째 슬라이드 버튼 추가

--- src/components/common/button/button.styles.ts
+++ src/components/common/button/button.styles.ts
@@ -1,11 +1,10 @@
 import styled from '@emotion/styled';
 import { colors } from '../../../style/colors';
-
+import { typography } from '../../../style/theme';
 export const BaseButton = styled.button<{ variant?: 'primary' | 'gray' }>`
   background-color: ${({ variant }) =>
     variant === 'primary' ? colors.green[200] : colors.gray[10]};
   color: ${colors.gray[800]};
-
   display: flex;
   font-style: normal;
   font-weight: 700;
@@ -40,20 +39,41 @@ export const ButtonContainer = styled(BaseButton)<{ $size?: 'small' | 'medium' |
   }
 `;
 
-export const HeroBannerButton = styled(BaseButton)`
+export const HeroBannerBaseButton = styled.button`
   position: absolute;
-  bottom: 45px;
+  display: flex;
+  font-style: normal;
+  font-weight: 700;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap;
+  cursor: pointer;
   border-radius: 0.65156rem;
-  right: 20px;
+  padding: 0.87rem 1.3rem;
   z-index: 10;
-  display: inline-flex;
-
-  height: 2.3rem;
   font-size: 0.8145rem;
   line-height: 1.19456rem; /* 146.667% */
   letter-spacing: -0.00813rem;
-  padding: 0.86875rem 1.30313rem;
-  justify-content: center;
-  align-items: center;
-  gap: 0.543rem;
+
+  height: 2.3rem;
+`;
+export const HeroBannerSlideOneButton = styled(HeroBannerBaseButton)`
+  position: absolute;
+  bottom: 11.25%;
+  background-color: ${colors.green[200]};
+  right: 5%;
+  bottom: 15%;
+`;
+
+export const HeroBannerSlideTwoButton = styled(HeroBannerBaseButton)`
+  top: 35%;
+  right: 33%;
+  background-color: ${colors.gray[800]};
+  color: ${colors.white};
+`;
+export const HeroBannerSlideThreeButton = styled(HeroBannerBaseButton)`
+  top: 45%;
+  right: 33%;
+  background-color: ${colors.green[200]};
 `;
