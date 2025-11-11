32bde60cba23a66197aa16f2a71ae9a528e6ac46
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-11 16:08:48 +0900

    refactor: 에러 폴백 페이지 에러 상태 초기화

--- src/error/QueryErrorBoundary.tsx
+++ src/error/QueryErrorBoundary.tsx
@@ -1,17 +1,31 @@
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 import type { PropsWithChildren } from 'react';
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
 import ErrorFallback from './ErrorFallback';
 
 export default function QueryErrorBoundary({ children }: PropsWithChildren) {
   const { reset } = useQueryErrorResetBoundary();
+  const location = useLocation();
+  const resetRef = useRef<(() => void) | null>(null);
+
+  const handleReset = () => {
+    reset();
+  };
+
+  useEffect(() => {
+    if (resetRef.current) {
+      resetRef.current();
+    }
+  }, [location.pathname, location.key]);
 
   return (
     <ErrorBoundary
-      onReset={reset}
+      onReset={handleReset}
       fallback={(props) => {
         const { error } = props;
-        return <ErrorFallback error={error} />;
+        return <ErrorFallback error={error} resetRef={resetRef} />;
       }}
     >
       {children}
